cd85a03598f9cf1cfa24fa56fa4c58803f556c03
Eugene Wang <eugene.r.w.12@gmail.com>
2017-02-28 18:37:00 -0500

    added grid

--- app/Dashboard/HomeScreen.js
+++ app/Dashboard/HomeScreen.js
@@ -1,36 +1,66 @@
 //Vire org
  import React, {Component} from 'react';
 
-import {
-     Text,
-     View,
-     ListView,
-     Image
+// https://www.npmjs.com/package/react-native-smart-sudoku-grid
 
+
+import {
+    ScrollView,
+    StyleSheet,
+    View,
+    Image,
+    Text,
+    TouchableHighlight,
+    Alert,
 } from 'react-native';
 
+import SudokuGrid from 'react-native-smart-sudoku-grid'
+import CornerLabel from 'react-native-smart-corner-label'
 import styles from './HomeScreenStyle';
 import GridView from 'react-native-grid-view';
+import image_google from '../img/g.png'
 
-var ITEMS_PER_ROW = 3;
+const dataList = [
+  {
+    icon: image_google,
+    title: 'cash',
+  },
+  {
+    icon: image_google,
+    title: 'cash',
+  },
+  {
+    icon: image_google,
+    title: 'cash',
+  },
+  {
+    icon: image_google,
+    title: 'cash',
+  },
+  {
+    icon: image_google,
+    title: 'cash',
+  },
+  {
+    icon: image_google,
+    title: 'cash',
+  },
+  {
+    icon: image_google,
+    title: 'cash',
+  },
+  {
+    icon: image_google,
+    title: 'cash',
+  },
+  {
+    icon: image_google,
+    title: 'cash',
+  },
+]
 
+var columnCount = 3;
 
-
-class Movie extends Component {
-  render() {
-      return (
-        <View style={styles.movie} >
-
-          <View >
-            <Text
-            style={styles.title}
-            numberOfLines={3}>{this.props.movie.title}</Text>
-            <Text style={styles.year}>{this.props.movie.year}</Text>
-          </View>
-        </View>
-      );
-  }
-}
 
 
 
@@ -39,7 +69,8 @@ class Movie extends Component {
          super(props);
          this.state = {
            loaded: true,
-           dataSource: ["Yolo", "haha", "xixi", "Hehe"]
+           dataSource : "Yolo"
+
 
          };
      }
@@ -56,12 +87,17 @@ class Movie extends Component {
 
        return (
            <View style={styles.toplevel}>
-           <GridView
-             items={this.state.dataSource}
-             itemsPerRow={ITEMS_PER_ROW}
-             renderItem={this.renderItem}
-             style={styles.listView}
-           />
+           <ScrollView style={{marginTop: 44 + 20, backgroundColor: '#fff', }}>
+             <View style={{height: 30, paddingLeft: 10, backgroundColor: '#E1E5E8', justifyContent: 'center', }}>
+                 <Text>Services: </Text>
+             </View>
+             <SudokuGrid
+                 containerStyle={{ backgroundColor: '#fff',}}
+                 columnCount={columnCount}
+                 dataSource={dataList}
+                 renderCell={this._renderGridCell}
+             />
+         </ScrollView>
            </View>
        );
      }
@@ -76,9 +112,38 @@ class Movie extends Component {
        );
      }
 
-     renderItem(item) {
-        return <Movie movie={item} />
+     _renderGridCell = (data, index, list) => {
+         return (
+             <TouchableHighlight underlayColor={'#eee'} onPress={ this._onPressCell.bind(this, data, index) }>
+                 <View style={{ overflow: 'hidden',
+                           justifyContent: 'center', alignItems: 'center', height: 100,
+                           borderBottomWidth: StyleSheet.hairlineWidth, borderColor: '#eee',
+                           borderRightWidth: (index + 1) % columnCount ? StyleSheet.hairlineWidth: 0, }}>
+                     <Image source={data.icon} style={{width: 30, height: 30, marginHorizontal: 10, marginBottom: 10,}}/>
+                     <Text>{data.title}</Text>
+                     { index == 6 ?
+                         <CornerLabel
+                             cornerRadius={54}
+                             style={{backgroundColor: 'red', height: 24,}}
+                             textStyle={{color: '#fff', }}>
+                             30% off
+                         </CornerLabel> : index == 3 ?
+                             <CornerLabel
+                                 alignment={'right'}
+                                 cornerRadius={54}
+                                 style={{backgroundColor: 'red', height: 24,}}
+                                 textStyle={{color: '#fff', fontSize: 12,}}>
+                                 Yolo
+                             </CornerLabel> : null
+                     }
+                 </View>
+             </TouchableHighlight>
+         )
      }
+
+     _onPressCell (data, index) {
+        Alert.alert('clicked ' + data.title)
+    }
 
 
 
