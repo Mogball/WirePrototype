072227d371692acbd6ffd118ad3f4497a6a8e145
Eugene Wang <eugene.r.w.12@gmail.com>
2017-02-28 15:37:14 -0500

    list functioning, to work on grid

--- app/Dashboard/HomeScreen.js
+++ app/Dashboard/HomeScreen.js
@@ -1,20 +1,47 @@
 //Vire org
  import React, {Component} from 'react';
 
- import {
+import {
      Text,
      View,
-     ListView
+     ListView,
+     Image
 
- } from 'react-native';
+} from 'react-native';
 
- import styles from './HomeScreenStyle';
+import styles from './HomeScreenStyle';
+import GridView from 'react-native-grid-view';
+
+var ITEMS_PER_ROW = 3;
+
+
+
+class Movie extends Component {
+  render() {
+      return (
+        <View style={styles.movie} >
+
+          <View >
+            <Text
+            style={styles.title}
+            numberOfLines={3}>{this.props.movie.title}</Text>
+            <Text style={styles.year}>{this.props.movie.year}</Text>
+          </View>
+        </View>
+      );
+  }
+}
+
 
 
  export default class HomeScreen extends Component {
      constructor(props) {
          super(props);
-         this.state = {loaded: true};
+         this.state = {
+           loaded: true,
+           dataSource: ["Yolo", "haha", "xixi", "Hehe"]
+
+         };
      }
 
 
@@ -29,7 +56,12 @@
 
        return (
            <View style={styles.toplevel}>
-
+           <GridView
+             items={this.state.dataSource}
+             itemsPerRow={ITEMS_PER_ROW}
+             renderItem={this.renderItem}
+             style={styles.listView}
+           />
            </View>
        );
      }
@@ -42,6 +74,12 @@
            </Text>
          </View>
        );
- }
+     }
+
+     renderItem(item) {
+        return <Movie movie={item} />
+     }
+
+
 
  }
