720a0b629e98122829dc2f8863ed24bcb4914f23
Eugene Wang <eugene.r.w.12@gmail.com>
2017-02-28 20:00:19 -0500

    xd

--- android/app/src/main/java/com/wireprototype/MainApplication.java
+++ android/app/src/main/java/com/wireprototype/MainApplication.java
@@ -4,6 +4,9 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
+import com.horcrux.svg.SvgPackage;
+import com.horcrux.svg.SvgPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -27,6 +30,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
+            new SvgPackage(),
+            new SvgPackage(),
             new RCTCameraPackage(),
             new SvgPackage()
       );
