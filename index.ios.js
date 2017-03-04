b8597599bd4dfbb137b435a9cd8008686e7c4b53
Eugene <eugene.r.w.12@gmail.com>
2017-03-03 19:27:16 -0800

    apple side of project built

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,86 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, {Component} from 'react';
 
-import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    Navigator
 } from 'react-native';
 
-export default class WirePrototype extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import LoadingScreen from './app/Launch/LoadingScreen';
+import LaunchScreen from './app/Launch/LaunchScreen';
+import LoginScreen from './app/Launch/LoginScreen';
+import RegisterScreen from './app/Launch/RegisterScreen';
+import Dashboard from './app/Dashboard';
+import DashboardHistoryScreen from './app/DashboardHistoryScreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+const routes = [
+    {title: 'LoadingScreen', index: 0},
+    {title: 'LaunchScreen', index: 1},
+    {title: 'LoginScreen', index: 2},
+    {title: 'RegisterScreen', index: 3},
+    {title: 'Dashboard', index: 4}
+];
+
+import * as firebase from 'firebase';
+import SessionModel from './app/Models/SessionModel';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBr1cBlr5gRDua1nPQd600RFj0T-UcEbVE",
+    authDomain: "vire-e9eb3.firebaseapp.com",
+    databaseURL: "https://vire-e9eb3.firebaseio.com",
+    storageBucket: "vire-e9eb3.appspot.com"
 });
+
+SessionModel.get().setFirebase(firebase);
+
+class WirePrototype extends Component {
+
+    render() {
+        return (
+            <Navigator
+                initialRoute={routes[0]}
+                configureScene={(route) => {
+                    if (route.title === 'LoadingScreen' || route.title === 'LaunchScreen') {
+                        return {
+                            ...Navigator.SceneConfigs.FadeAndroid,
+                            gestures: {}
+                        }
+                    }
+                    return {
+                        ...Navigator.SceneConfigs.PushFromRight,
+                        gestures: {}
+                    };
+                }}
+                renderScene={(route, navigator) => {
+                    if (route.title == 'LoadingScreen') {
+                        return (
+                            <LoadingScreen navigator={navigator}/>
+                        );
+                    } else if (route.title == 'LaunchScreen') {
+                        return (
+                            <LaunchScreen navigator={navigator}/>
+                        );
+                    } else if (route.title == 'LoginScreen') {
+                        return (
+                            <LoginScreen navigator={navigator}/>
+                        );
+                    } else if (route.title == 'RegisterScreen') {
+                        return (
+                            <RegisterScreen navigator={navigator}/>
+                        );
+                    } else if (route.title == 'Dashboard') {
+                        return (
+                            <Dashboard ref='dashboard' navigator={navigator} route={route}/>
+                        );
+                    } else if (route.title == 'DashboardHistoryScreen') {
+                        return (
+                            <DashboardHistoryScreen dashboard={this.refs.dashboard}/>
+                        );
+                    }
+                }}
+            />
+        );
+    }
+
+}
 
 AppRegistry.registerComponent('WirePrototype', () => WirePrototype);
